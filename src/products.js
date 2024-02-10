e516ba4cc6f190ec71bcf2b1bbec73588bf78bad
Ahmed Idris <ahmedidris@ahmeds-mbp-2.mynetworksettings.com>
2024-02-10 15:55:32 -0500

    First Commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/swy.png";
+import product2 from "./assets/products/valentino.png";
+import product3 from "./assets/products/spicebomb.png";
+import product4 from "./assets/products/azzaro.png";
+import product5 from "./assets/products/replica.png";
+import product6 from "./assets/products/jpg.png";
+import product7 from "./assets/products/xerjoff.png";
+import product8 from "./assets/products/khamrah.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Stronger With You Intensely Eau De Parfum",
+    price: 129.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Uomo Born in Roma Eau de Toilette",
+    price: 97.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Spicebomb Extreme Eau De Parfum",
+    price: 86.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Azzaro The Most Wanted Parfum",
+    price: 125.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "REPLICA' Jazz Club Eau de Toilette",
+    price: 85.00,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "JEAN PAUL GAULTIER Le Male Elixir",
+    price: 152.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "XERJOFF Erba Pura Eau de Parfum",
+    price: 250.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Lattafa Khamrah Eau de Parfum",
+    price: 26.0,
+    productImage: product8,
+  },
+];
